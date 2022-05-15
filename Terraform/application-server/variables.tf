d91d578d19bf9e4d6ee213659c1c5e78b8973cca
Ваше Имя <you@example.com>
2022-05-15 21:52:14 +0300

    Implemented First Part of Terraform

---
+++ Terraform/application-server/variables.tf
@@ -0,0 +1,33 @@
+variable "ami-id" {
+type = string
+}
+
+variable "ami-instance-profile" {
+  default = ""
+  type    = string
+}
+
+variable "instance-type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "name" {
+  type = string
+}
+
+variable "key-pair" {
+  type = string
+}
+
+variable "network-interface-id" {
+  type = string
+}
+
+variable "device-index" {
+  type = string
+}
+
+variable "repository-url" {
+  type = string
+}
