1b744a8a616f2a049024f79d95bb9a9dc87769d0
Malik Danil <danilmalik12345@gmail.com>
2022-11-08 03:56:29 +0300

    update

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -9,3 +9,31 @@ type = string
 variable "aws-region" {
 type = string
 }
+
+variable "admin-username" {
+  type = string
+}
+
+variable "admin-password" {
+  type = string
+}
+
+variable "admin-fullname" {
+  type = string
+}
+
+variable "admin-email" {
+  type = string
+}
+
+variable "remote-repo" {
+  type = string
+}
+
+variable "job-name" {
+  type = string
+}
+
+variable "secrets" {
+  type = map(string)
+}
