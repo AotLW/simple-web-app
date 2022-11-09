ebdb897d3e891bdde3296dc355fbead204936784
Malik Danil <danilmalik12345@gmail.com>
2022-11-10 00:38:26 +0300

    Implemented Second Part of Terraform + debug

--- Terraform/jenkins-server/variables.tf
+++ Terraform/jenkins-server/variables.tf
@@ -1,14 +1,14 @@
 variable "ami-id" {
-type = string
+  type = string
 }
 
 variable "iam-instance-profile" {
   default = ""
-  type    = string
+  type = string
 }
 
 variable "instance-type" {
-  type    = string
+  type = string
   default = "t2.micro"
 }
 
@@ -25,9 +25,61 @@ variable "network-interface-id" {
 }
 
 variable "device-index" {
-  type = string
+  type = number
 }
 
 variable "repository-url" {
+  type = string
+}
+
+variable "repository-test-url" {
+  type = string
+}
+
+variable "repository-staging-url" {
+  type = string
+}
+
+variable "instance-id" {
+  type = string
+}
+
+variable "public-dns" {
+  type = string
+}
+
+variable "admin-username" {
+  type = string
+}
+
+variable "admin-password" {
+  type = string
+}
+
+variable "admin-email" {
+  type = string
+}
+
+variable "admin-fullname" {
+  type = string
+}
+
+variable "bucket-logs-name" {
+  type = string
+}
+
+variable "bucket-config-name" {
+  type = string
+}
+
+variable "remote-repo" {
+  type = string
+}
+
+variable "job-name" {
+  type = string
+}
+
+variable "job-id" {
   type = string
 }
